18b262ab5fc0cdb3ab48f9e03ce8f8f16106883a
Suyash Pathak <pathaksuyash04113@gmail.com>
2025-08-26 19:13:37 +0530

    cartdetail css2.6

--- fruit_shop/settings.py
+++ fruit_shop/settings.py
@@ -160,8 +160,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+import os
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
